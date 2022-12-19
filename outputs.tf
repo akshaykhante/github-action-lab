59e1de2a8d24706fc1b059684228b6e5350c8424
unknown <akshaykhante25@gmail.com>
2022-12-19 17:49:18 +0530

    Added terraform script and updated workflow to plan and apply it

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+# output "instance_arn" {
+#   value = [for i in aws_instance.test_instance: i.arn]
+# }
+
+# output "instance_public_dns" {
+#   value = [for i in aws_instance.test_instance: i.public_dns]
+# }
